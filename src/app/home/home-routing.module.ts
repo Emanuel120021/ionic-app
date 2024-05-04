212f8d7e905a49c655ecc4a955b58fe8625beab3
Emanuel120021 <emanuel.lazaro.52@gmail.com>
2024-05-04 17:43:40 -0300

    inicial commit

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -6,11 +6,27 @@ const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'calculadora',
+        loadChildren: () =>
+          import('../calculadora/calculadora.module').then(
+            (m) => m.CalculadoraPageModule
+          ),
+      },
+      {
+        path: 'calendario',
+        loadChildren: () =>
+          import('../calendario/calendario.module').then(
+            (m) => m.CalendarioPageModule
+          ),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class HomePageRoutingModule {}
